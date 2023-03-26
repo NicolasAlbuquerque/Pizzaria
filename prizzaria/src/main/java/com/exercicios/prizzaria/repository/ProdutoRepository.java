21c5012c8f77f0861c5d93654c22b1f3107553c8
NICOLAS ALBUQUERQUE <123500218+NicolasAlbuquerque@users.noreply.github.com>
2023-03-26 13:22:13 -0300

    Criação da camada Repository

---
+++ prizzaria/src/main/java/com/exercicios/prizzaria/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.exercicios.prizzaria.repository;
+
+import com.exercicios.prizzaria.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
